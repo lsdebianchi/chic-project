aabe6483afde047b0f3c403936edb20169d18736
Nathan Vogel <nathan@nathanvogel.com>
2018-09-27 19:52:26 +0200

    Fix unresponsive buttons

--- public/js/vh_fix_fucking_safari.js
+++ public/js/vh_fix_fucking_safari.js
@@ -1,7 +1,7 @@
-
-var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
-var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-if (isSafari && iOS) {
-  $('.body').append("<div class='banner'><a>Sorry, </br>Safari is not supported. </br>Open this website in </br>Chrome or Firefox.</a></div>");
-}
+//
+// var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+// var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+//
+// if (isSafari && iOS) {
+//   $('.body').append("<div class='banner'><a>Sorry, </br>Safari is not supported. </br>Open this website in </br>Chrome or Firefox.</a></div>");
+// }
